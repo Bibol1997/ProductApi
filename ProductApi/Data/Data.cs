75276b34fbe720d57a963523ce37659d1c0b60cb
Yergesh <Yergesh@HOME-PC>
2023-10-16 06:33:16 +0300

    Добавьте файлы проекта.

---
+++ ProductApi/Data/Data.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductApi.Model;
+
+namespace ProductApi
+{
+    public class Data : DbContext
+    {
+        public Data (DbContextOptions<Data> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductApi.Model.Product> Product { get; set; }
+        public DbSet<ProductApi.Model.Product> Category { get; set; }
+    }
+}
---
+++ ProductApi/Model/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductApi.Model
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? lastName { get; set; }
+    }
+}
